01d903026e28b78bf28021aa3a33e7d826c7cda9
sforsanjnarao <sanjnarao26@gmail.com>
2025-06-23 17:14:29 +0530

    Add utility functions and UI components for improved styling and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope} from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/provider/theme-provider";
 
-const geistSans = Geist({
+const manrope = Manrope({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +20,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    <html lang="en" suppressHydrationWarning>
+      <body 
+      suppressHydrationWarning
+        className={` ${manrope.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+              {children}
+          </ThemeProvider>
+
+        
       </body>
     </html>
   );

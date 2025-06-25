28292ea1f554a112bae92ebe4bc339d42ea78c5d
sforsanjnarao <sanjnarao26@gmail.com>
2025-06-25 22:59:26 +0530

    working store

--- src/store/useWebinarStore.ts
+++ src/store/useWebinarStore.ts
@@ -1,22 +1,152 @@
+import { CtaTypeEnum } from "@/generated/prisma";
+import { validateCTA, ValidationErrors, validateBasicInfo, ValidationResult } from "@/lib/type";
 import { create } from "zustand";
+type WebinarFormState = {
+    // create webinar modal type
+    basicInfo: {
+        webinarName?: string
+        description?: string
+        date?: string
+        time?: string
+        timeFormat?: 'AM' | 'PM'
+    }
+    cta:{
+        ctaLabel?: string
+        tags?: string[]
+        ctaType: CtaTypeEnum
+        aiAgent?: string
+        priceId?: string
+    }
+    additionalInfo: {
+        lockCta?: boolean
+        couponCode?: string
+        couponEnabled?: boolean
+    }
+}
+type ValidityState = {
+    basicInfo: {
+       valid: boolean
+       errors:ValidationErrors
+    }
+    cta:{
+        valid: boolean
+        errors:ValidationErrors
+    }
+    additionalInfo: {
+        valid: boolean
+       errors:ValidationErrors   
+    }
+}
 
 type WebinarStore = {
     isModalOpen: boolean;
     isComplete: boolean;
     isSubmitting: boolean;
+    formData: WebinarFormState;
+    validation:ValidityState
+    
 
     setModalOpen:(open: boolean) => void;
     setComplete: (complete: boolean) => void;
     setSubmitting: (submitting: boolean) => void;
+
+    updateBasicInfoField: <K extends keyof WebinarFormState['basicInfo']>(
+        field:K, 
+        value: WebinarFormState['basicInfo'][K]
+    )=> void
+
+    updateCtaField: <K extends keyof WebinarFormState['cta']>(
+        field:K, 
+        value: WebinarFormState['cta'][K]
+    )=> void
+    updateAdditionalInfoField: <K extends keyof WebinarFormState['additionalInfo']>(
+        field:K, 
+        value: WebinarFormState['additionalInfo'][K]
+    )=> void
+
+    validateStep:(stepId: keyof WebinarFormState) => boolean
+    getStepValidationErrors: (stepId: keyof WebinarFormState) => ValidationErrors 
+
+    resetForm: () => void
+
 }
+
+const initialState: WebinarFormState = {
+    basicInfo: {
+        webinarName: '',
+        description: '',
+        date: undefined,
+        time: '',
+        timeFormat: "AM"
+    },
+    cta: {
+        ctaLabel: '',
+        tags: [],
+        ctaType: "BOOK_A_CALL",
+        aiAgent: '',
+        priceId: ''
+    },
+    additionalInfo: {
+        lockCta: false,
+        couponCode: '',
+        couponEnabled: false
+    }
+}
+
+const initialValidation: ValidityState = {
+    basicInfo: { valid: false, errors: {}},
+    cta: { valid: false, errors: {}},
+    additionalInfo: { valid: false,errors: {}}
+}
+
 export const useWebinarStore = create<WebinarStore>((set) => ({
     isModalOpen: false,
     isComplete: false,
     isSubmitting: false,
+    formData: initialState,
+    validation: initialValidation,
+
 
     setModalOpen: (open:boolean) => set({ isModalOpen: open }),
     setComplete: (complete:boolean) => set({ isComplete: complete }),
     setSubmitting: (submitting:boolean) => set({ isSubmitting: submitting})
 
+    upadateBasicInfoField:(field, value)=>{
+        set((state)=>{
+            const newBasicInfo = {...StaticRange.formData.basicInfo, [field]: value};
+            const validationResult = validateBasicInfo(newBasicInfo);
 
+            return {
+                formData: {
+                    ...state.formData,
+                    basicInfo: newBasicInfo
+                },
+                validation: {
+                    ...state.validation,
+                    basicInfo: validationResult
+                        
+                }
+            };
+        })
+    }
+    updateCtaField:(field, value)=> {
+        set((state) => {
+            const newCta = { ...state.formData.cta, [field]: value };
+            const validationResult = validateCTA(newCta);
+
+            return {
+                formData: {
+                    ...state.formData,
+                    cta: newCta
+                },
+                validation: {
+                    ...state.validation,
+                    cta: validationResult
+                }
+            };
+    })
+        
+    },
+
+    upd
 }));
